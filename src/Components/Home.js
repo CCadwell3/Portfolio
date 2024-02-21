6f9ffad46103d8a23489a30026f8ff89ed7fb000
Christopher Cadwell <CCadwell3@Gmail.com>
2024-02-21 13:20:23 -0700

    Basic layout

---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to My Game Programming Portfolio</h1>
+            <p>Here you'll find projects I've worked on, my skills, and how to contact me.</p>
+        </div>
+    );
+};
+
+export default Home;
