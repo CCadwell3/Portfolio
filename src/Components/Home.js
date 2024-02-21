cd0411ee746a1d22ba4afccce1c18c70842c25cc
Christopher Cadwell <CCadwell3@Gmail.com>
2024-02-21 13:28:26 -0700

    update

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Home.css'; // Link to the CSS file for styling
 
 const Home = () => {
     return (
-        <div>
+        <div className="home">
             <h1>Welcome to My Game Programming Portfolio</h1>
-            <p>Here you'll find projects I've worked on, my skills, and how to contact me.</p>
+            <p>I am a dedicated game programming student at the University of Advancing Technology (UAT), on the cusp of completing my Bachelor of Science degree. This portfolio showcases the projects and skills I've honed throughout my academic journey, highlighting my passion for game development and my readiness to contribute to the field.</p>
         </div>
     );
 };
