0ea8a204e23a0bc957f0620e3d09f20a19a1939a
Christopher Cadwell <CCadwell3@Gmail.com>
2024-02-22 07:42:52 -0700

    updated background, css, and some components

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Home.css'; // Link to the CSS file for styling
+import backgroundImage from './Img/bkg.jpg';
 
 const Home = () => {
     return (
-        <div className="home">
+        <div className="hero">
             <h1>Welcome to My Game Programming Portfolio</h1>
+            <br></br>
             <p>I am a dedicated game programming student at the University of Advancing Technology (UAT), on the cusp of completing my Bachelor of Science degree. This portfolio showcases the projects and skills I've honed throughout my academic journey, highlighting my passion for game development and my readiness to contribute to the field.</p>
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './global.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import backgroundImage from './Components/Img/bkg.jpg';
+
+// Directly set the background style on the body element
+document.body.style.backgroundImage = `url(${backgroundImage})`;
+document.body.style.margin = '0';
+document.body.style.padding = '0';
+document.body.style.backgroundSize = 'cover';
+document.body.style.backgroundPosition = 'center';
+document.body.style.backgroundAttachment = 'fixed';
+document.body.style.width = '100%';
+document.body.style.height = '100%';
+document.body.style.overflowX = 'hidden';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
